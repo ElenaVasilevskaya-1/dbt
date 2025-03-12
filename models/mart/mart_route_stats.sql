ed4b7339aae6b9a9264ee80a765a32dd418ba71b
Elena Vasilevskaya <elenavasilevskaya@MacBook-Air-Elena.local>
2025-03-12 11:57:15 +0100

    lalal

--- models/mart/mart_route_stats.sql
+++ models/mart/mart_route_stats.sql
@@ -1,30 +1,26 @@
-
-SELECT * FROM PREP_FLIGHTS;
-SELECT * FROM PREP_AIRPORTS; 
 WITH flight_route_stats AS (
-					SELECT 
-					origin
-					,dest
-					,count(*)
-					,count(DISTINCT tail_number) AS nunique_tails
-					,count(DISTINCT airline) AS num_of_airlines
-					,max(arr_delay_interval) AS max_arr_delay
-					,min(arr_delay_interval) AS min_arr_delay
-					,sum(cancelled) AS total_canceled
-					,sum(diverted) AS total_diverted 
-					FROM {{ref('prep_flights')}}
-					GROUP BY (dest, origin)
+        SELECT 
+            origin 
+            ,dest
+            ,COUNT(flight_number) AS n_flights
+            ,COUNT(DISTINCT tail_number) AS nunique_tails
+            ,COUNT(DISTINCT airline) AS nunique_airlines
+            ,MAX(arr_delay_interval) AS max_arr_delay
+            ,MIN(arr_delay_interval) AS min_arr_delay
+            ,SUM(cancelled) AS total_canceled
+            ,SUM(diverted) AS total_diverted
+        FROM {{ref('prep_flights')}}
+        GROUP BY (origin, dest) 
 )
-SELECT o.city AS origin_city,
-		d.city AS dest_city,
-		o.name AS origin_name,
-		d.name AS dest_name,
-		o.country AS origin_country,
-		d.country AS dest_country,
-		f.*
+SELECT o.city AS origin_city, 
+        d.city AS dest_city,
+        o.name AS origin_name,
+        d.name AS dest_name,
+        o.country AS origin_country, 
+        d.country AS dest_country,
+        f.*
 FROM flight_route_stats f
 LEFT JOIN {{ref('prep_airports')}} o
-	ON f.origin=o.faa
-LEFT join {{ref('prep_airports')}} d
-	ON f.dest=d.faa
-
+    ON f.origin=o.faa
+LEFT JOIN {{ref('prep_airports')}} d
+    ON f.dest=d.faa
